a7df18df7bead2aaf8a3991665a2e4558340af46
Roger Wood <Roger.wood@gmail.com>
2012-11-06 19:43:15 -0700

    First commit

---
+++ CSVApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSVApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            IEnumerable<string> lines = File.ReadLines("Input.csv");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                DataGridViewRow row = new DataGridViewRow();
+                
+            }
+
+            
+
+        }
+    }
+}
